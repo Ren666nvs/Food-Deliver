03440d593a2e037c6e4b561b346a8c87a92632bc
Ren666nvs <164177806+Ren666nvs@users.noreply.github.com>
2025-02-23 21:11:51 +0800

    a

--- src/controller/users/create-movie.js
+++ src/controller/users/create-movie.js
@@ -8,3 +8,20 @@ export const createMovie = (req, res) => {
     fs.writeFileSync("src/DB/movies.json", JSON.stringify(movies));
     res.send(req.body);
  };
+ import { log } from "console";
+import fs from "fs";
+
+export const deleteMovie = (req, res) => {
+    const rawMovies = fs.readFileSync("src/DB/movies.json");
+    console.log(rawMovies);
+    
+    const movies = JSON.parse(rawMovies);
+    if (movies.length > 0) {
+        const removedMovie = movies.pop();
+        res.json(removedMovie);
+        fs.writeFileSync("src/DB/movies.json", JSON.stringify(movies));
+    } else {
+        res.json({ message: "No movies to delete" });
+    }
+    fs.writeFileSync("src/DB/movies.json", JSON.stringify(movies));
+};
